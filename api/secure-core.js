3874a7c618e8427a63dcad42c89e0a845856be14
kenmhnck-ctrl <ken.m.h.n.ck@gmail.com>
2026-09-10 23:58:34 +0900

    Enforce async tenant auth on secure core

--- api/secure-core.js
+++ api/secure-core.js
@@ -1,3 +1,3 @@
 import core from '../lib/core.js';
 import { enforceTenant } from './_auth.js';
-export default async function handler(req,res){if(!enforceTenant(req,res))return;return core(req,res)}
+export default async function handler(req,res){const a=await enforceTenant(req,res,{roles:['admin','owner','editor','member','viewer']});if(!a)return;return core(req,res)}
